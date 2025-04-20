1f6524a8751a9944924216d9d02585ebcf4271ec
enk-it <gladysh.danil@gmail.com>
2025-04-20 16:17:18 +0700

    feat: Update docs for request schema

--- src/main/java/com/example/backend/DTO/JwtRequest.java
+++ src/main/java/com/example/backend/DTO/JwtRequest.java
@@ -1,6 +1,8 @@
 package com.example.backend.DTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,13 @@ import lombok.Setter;
 @Data
 @Schema(description = "Запрос на вход")
 public class JwtRequest {
+    @Schema(description = "Логин пользователя", example = "gladyshdd")
+    @Size(min = 5, max = 255, message = "Логин должен содержать от 5 до 255 символов")
+    @NotBlank(message = "Имя пользователя не может быть пустыми")
     private String username;
+
+    @Schema(description = "Пароль пользователя", example = "ASDhj91ahsd")
+    @Size(min = 5, max = 255, message = "Пароль должен содержать от 5 до 255 символов")
+    @NotBlank(message = "Пароль не может быть пустым")
     private String password;
 }
