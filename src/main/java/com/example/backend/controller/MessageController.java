fba51613d6f18804ec807a652de824f8d1b68a11
enk-it <gladysh.danil@gmail.com>
2025-04-24 00:37:09 +0700

    reafactor

--- src/main/java/com/example/backend/controller/MessageController.java
+++ src/main/java/com/example/backend/controller/MessageController.java
@@ -9,8 +9,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
