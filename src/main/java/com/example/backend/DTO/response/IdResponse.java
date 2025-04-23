cf7c2d199d0418a8ae77b21ce1315eda6955ed1b
enk-it <gladysh.danil@gmail.com>
2025-04-24 00:36:06 +0700

    refactor

--- src/main/java/com/example/backend/DTO/response/IdResponse.java
+++ src/main/java/com/example/backend/DTO/response/IdResponse.java
@@ -1,12 +1,9 @@
 package com.example.backend.DTO.response;
 
-import com.example.backend.model.chat.ChatType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
