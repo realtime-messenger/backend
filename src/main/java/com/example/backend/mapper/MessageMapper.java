2cc6421d8d9e10f709bf6a46628a24f079fbb79d
enk-it <gladysh.danil@gmail.com>
2025-04-24 00:35:58 +0700

    feat: Update mapper
    Message now have UserResponse instead of userId

--- src/main/java/com/example/backend/mapper/MessageMapper.java
+++ src/main/java/com/example/backend/mapper/MessageMapper.java
@@ -3,6 +3,7 @@ package com.example.backend.mapper;
 import com.example.backend.DTO.response.MessageResponse;
 import com.example.backend.DTO.response.MessageExtendedResponse;
 import com.example.backend.model.message.Message;
+import com.example.backend.model.user.User;
 import com.example.backend.model.userMessageReaction.UserMessageReaction;
 import com.example.backend.model.userMessageStatus.UserMessageStatus;
 import org.mapstruct.Mapper;
@@ -20,20 +21,22 @@ public interface MessageMapper {
     MessageResponse toMessageResponse(Message message);
 
     @Mapping(target = "id", source = "message.id")
-    @Mapping(target = "userId", source = "message.userId")
     @Mapping(target = "dateCreated", source = "message.dateCreated")
+    @Mapping(target = "user", source = "user")
     MessageExtendedResponse toMessageResponseExtended(
             Message message,
+            User user,
             UserMessageStatus userMessageStatus,
             Collection<UserMessageReaction> reactions
     );
 
     @Mapping(target = "id", source = "message.id")
-    @Mapping(target = "userId", source = "message.userId")
     @Mapping(target = "dateCreated", source = "message.dateCreated")
     @Mapping(target = "isRead", source = "isRead")
+    @Mapping(target = "user", source = "user")
     MessageExtendedResponse toMessageResponseExtended(
             Message message,
+            User user,
             boolean isRead,
             Collection<UserMessageReaction> reactions
     );
